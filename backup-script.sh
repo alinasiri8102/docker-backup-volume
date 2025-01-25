b34073ae31bbee497da4ece8e351b29e833b2f15
Ali Nasiri <alinasiri8102@gmail.com>
2025-01-25 15:18:00 +0330

    bug fix

--- backup-script.sh
+++ backup-script.sh
@@ -43,7 +43,7 @@ backup_volumes() {
         BACKUP_FILE="${BACKUP_DIR}/${volume}.tar.gz"
 
         docker run --rm -v "${volume}:/volume" -v backup-service_data:/backup alpine:latest \
-            /bin/sh -c "tar -czf /backup/$(basename "$BACKUP_FILE") -C / volume"
+            /bin/sh -c "tar -czf /backup/_data/$(basename "$BACKUP_FILE")"
 
         if [ ! -f "$BACKUP_FILE" ]; then
             echo "backup file $BACKUP_FILE was not created. Skipping this volume."
