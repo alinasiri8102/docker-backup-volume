a68db9e27250ae3037ca3f7f8cc60a22368251bb
Ali Nasiri <alinasiri8102@gmail.com>
2025-02-05 01:23:32 +0330

    add split

--- backup-script.sh
+++ backup-script.sh
@@ -5,24 +5,28 @@ mkdir -p "$BACKUP_DIR"
 
 DATE=$(date +"%Y-%m-%d_%H-%M-%S")
 RETENTION_DAYS=${BACKUP_RETENTION_DAYS:-14}
+CHUNK_SIZE=49M 
 
 send_telegram() {
     local archive_file=$1
     if [ -n "$TELEGRAM_BOT_TOKEN" ] && [ -n "$TELEGRAM_CHAT_ID" ]; then
-        curl_cmd="curl -s -X POST \"https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendDocument\" \
-            -F chat_id=\"${TELEGRAM_CHAT_ID}\" \
-            -F document=\"@${archive_file}\" \
-            -F caption=\"📌 $DATE\""
-        
-        if [ -n "$TELEGRAM_THREAD_ID" ]; then
-            curl_cmd="$curl_cmd -F message_thread_id=\"${TELEGRAM_THREAD_ID}\""
-        fi
-        response=$(eval "$curl_cmd")
-        if echo "$response" | grep -q '"ok":true'; then
-            echo "Backup successfully sent to Telegram."
-        else
-            echo "Error sending backup to Telegram: $response"
-        fi
+        for part in "$archive_file"*; do
+            curl_cmd="curl -s -X POST \"https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendDocument\" \
+                -F chat_id=\"${TELEGRAM_CHAT_ID}\" \
+                -F document=\"@${part}\" \
+                -F caption=\"📌 $DATE - Part: $(basename "$part")\""
+            
+            if [ -n "$TELEGRAM_THREAD_ID" ]; then
+                curl_cmd="$curl_cmd -F message_thread_id=\"${TELEGRAM_THREAD_ID}\""
+            fi
+            
+            response=$(eval "$curl_cmd")
+            if echo "$response" | grep -q '"ok":true'; then
+                echo "Backup part $(basename "$part") successfully sent to Telegram."
+            else
+                echo "Error sending backup part $(basename "$part") to Telegram: $response"
+            fi
+        done
     else
         echo "Telegram bot token or chat ID is not set. Skipping Telegram notification."
     fi
@@ -32,11 +36,11 @@ backup_volumes() {
     echo "Taking backups..."
 
     volumes=$(docker volume ls -q)
-    ARCHIVE_FILE="${BACKUP_DIR}/FullBackup_${DATE}.tar"
+    ARCHIVE_FILE="${BACKUP_DIR}/FullBackup_${DATE}.tar.gz"
     
     for volume in $volumes; do
         if [ "$volume" = "backup-service_data" ]; then
-            echo "skipping backup for volume $volume."
+            echo "Skipping backup for volume $volume."
             continue
         fi
 
@@ -46,28 +50,32 @@ backup_volumes() {
             /bin/sh -c "cd /volume && tar -czf /backup/$(basename "$BACKUP_FILE") ."
 
         if [ ! -f "$BACKUP_FILE" ]; then
-            echo "backup file $BACKUP_FILE was not created. Skipping this volume."
+            echo "Backup file $BACKUP_FILE was not created. Skipping this volume."
             continue
         fi
 
-        echo "backup completed for volume ${volume}: ${BACKUP_FILE}"
+        echo "Backup completed for volume ${volume}: ${BACKUP_FILE}"
 
         tar -rf "$ARCHIVE_FILE" -C "$BACKUP_DIR" "$(basename "$BACKUP_FILE")"
     done
 
     gzip "$ARCHIVE_FILE"
-    echo "compressed $ARCHIVE_FILE to ${ARCHIVE_FILE}.gz"
+    echo "Compressed $ARCHIVE_FILE to ${ARCHIVE_FILE}.gz"
 
-    send_telegram "${ARCHIVE_FILE}.gz"
+    if [ $(stat -c%s "${ARCHIVE_FILE}.gz") -gt 50000000 ]; then
+        split -b $CHUNK_SIZE -d "${ARCHIVE_FILE}.gz" "${ARCHIVE_FILE}.gz.part-"
+        rm "${ARCHIVE_FILE}.gz"
+        echo "Backup split into smaller parts."
+    fi
+
+    send_telegram "${ARCHIVE_FILE}.gz.part-"
 }
 
 cleanup_old_backups() {
-    find "$BACKUP_DIR" -type f -name "*.tar.gz" -mtime +$RETENTION_DAYS -exec rm {} \;
+    find "$BACKUP_DIR" -type f -name "*.tar.gz*" -mtime +$RETENTION_DAYS -exec rm {} \;
     echo "Old backups cleaned up, older than ${RETENTION_DAYS} days"
 }
 
 echo "SCRIPT STARTED ON ${DATE}"
 backup_volumes
 cleanup_old_backups
-
-
