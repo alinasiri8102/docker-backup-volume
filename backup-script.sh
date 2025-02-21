f0fc9aa3dda1d4b4a126c4975cedc8f122fb3b4a
Ali Nasiri <alinasiri8102@gmail.com>
2025-02-21 11:40:03 +0330

    .

--- backup-script.sh
+++ backup-script.sh
@@ -48,8 +48,8 @@ backup_volumes() {
 
         BACKUP_FILE="${BACKUP_DIR}/${volume}.zip"
 
-        docker exec volume_backup /bin/sh -c "cd /volume && zip -r /backup/$(basename "$BACKUP_FILE") ."
-
+        docker run --rm -v "$volume":/data -v backup-service_data:/backup volume_backup \
+            /bin/sh -c "cd /data && zip -r /backup/$(basename "$BACKUP_FILE") ."
 
         if [ ! -f "$BACKUP_FILE" ]; then
             echo "Backup file $BACKUP_FILE was not created. Skipping this volume."
