9a0518fbc4dc9a345cc43986b42e633ad9f76dee
Ali Nasiri <alinasiri8102@gmail.com>
2025-02-21 11:49:47 +0330

    clean up

--- backup-script.sh
+++ backup-script.sh
@@ -4,7 +4,7 @@ BACKUP_DIR="/backups"
 mkdir -p "$BACKUP_DIR"
 
 DATE=$(date +"%Y-%m-%d_%H-%M-%S")
-RETENTION_DAYS=${BACKUP_RETENTION_DAYS:-14}
+RETENTION_DAYS=${BACKUP_RETENTION_DAYS:-7}
 
 send_telegram() {
     local archive_file=$1
@@ -15,13 +15,13 @@ send_telegram() {
         return
     fi
 
-    zip -s 49m -r "${base_name}.zip" "$archive_file"
+    zip -q -s 49m -r "${base_name}.zip" "$archive_file"
 
     for part in ${base_name}.z* ${base_name}.zip; do
         if curl -s -X POST "https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendDocument" \
             -F chat_id="$TELEGRAM_CHAT_ID" \
             -F document="@$part" \
-            -F caption="${base_name} $(basename "$part")" \
+            -F caption="$(basename "$part")" \
             ${TELEGRAM_THREAD_ID:+-F message_thread_id="$TELEGRAM_THREAD_ID"} > /dev/null; then
             rm -f "$part"
         else
@@ -32,13 +32,13 @@ send_telegram() {
     echo "✅ Backup sent to Telegram."
 }
 
-
-
 backup_volumes() {
     echo "Taking backups..."
 
     volumes=$(docker volume ls -q)
     ARCHIVE_FILE="${BACKUP_DIR}/Backup_${DATE}.zip"
+
+    > "$ARCHIVE_FILE"
 
     for volume in $volumes; do
         if [ "$volume" = "backup-service_data" ]; then
@@ -49,7 +49,7 @@ backup_volumes() {
         BACKUP_FILE="${BACKUP_DIR}/${volume}.zip"
 
         docker run --rm -v "$volume":/data -v backup-service_data:/backup volume_backup \
-            /bin/sh -c "cd /data && zip -r /backup/$(basename "$BACKUP_FILE") ."
+            /bin/sh -c "cd /data && zip -q -r /backup/$(basename "$BACKUP_FILE") ."
 
         if [ ! -f "$BACKUP_FILE" ]; then
             echo "Backup file $BACKUP_FILE was not created. Skipping this volume."
@@ -58,7 +58,7 @@ backup_volumes() {
 
         echo "Backup completed for volume ${volume}: ${BACKUP_FILE}"
 
-        zip -r "$ARCHIVE_FILE" "$BACKUP_FILE"
+        zip -q "$ARCHIVE_FILE" "$BACKUP_FILE"
     done
 
     echo "Created archive: $ARCHIVE_FILE"
@@ -71,8 +71,6 @@ cleanup_old_backups() {
     echo "Old backups cleaned up, older than ${RETENTION_DAYS} days"
 }
 
-
 echo "SCRIPT STARTED ON ${DATE}"
 backup_volumes
 cleanup_old_backups
-
